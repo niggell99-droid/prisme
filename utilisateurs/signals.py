c5415ae150b0639ca6e841e293f512074bc4c9c0
jmd <jeanpaulmujinga64@gmail.com>
2026-05-10 07:36:11 +0200

    fix: disable automatic profile creation during loaddata to prevent IntegrityError

--- utilisateurs/signals.py
+++ utilisateurs/signals.py
@@ -6,12 +6,16 @@ from .models import Profil
 # Ce décorateur relie la fonction au signal 'post_save' du modèle User
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    if kwargs.get('raw', False):
+        return # Ignore si l'objet est importé via loaddata
     if created:
         # Si un nouvel utilisateur est créé (created=True), créez un profil vide
         Profil.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    if kwargs.get('raw', False):
+        return
     # À chaque sauvegarde d'utilisateur, sauvegarde également le profil lié
     # (Nécessaire pour le formulaire d'édition)
     if hasattr(instance, 'profil'):
